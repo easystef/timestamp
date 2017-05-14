803882210eb049dc77a85db499a49d42d2d09c3a
Stefan Hall <easystef@gmail.com>
2017-05-14 12:40:17 +0000

    Completed the exercise.

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 var express = require("express");
 
-var app = express()
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:inputDate', function (req, res) {
+  var unix;
+  var natural;
+  
+  // Check if input is natual or unix
+  unix = parseInt(req.params.inputDate);
+  
+  if (isNaN(unix)) {
+    
+    // If natural language date ...
+    unix = Date.parse(req.params.inputDate) / 1000;
+    natural = req.params.inputDate;
+    
+  // TODO different format for natural language date  
+    
+  } else {
+     
+    // If unix date ...
+    natural = new Date(unix * 1000);
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var mm = natural.getMonth();
+    var dd = natural.getDate();
+    var yy = natural.getFullYear();
+    natural = monthNames[mm] + ' ' + dd + ', ' + yy;
+    
+  }
+
+  res.send({unix, natural});
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('App listening on port 8080!')
 })
