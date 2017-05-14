a5ed580efe2b66be03882bd470f36f4378db2fd9
Stefan Hall <easystef@gmail.com>
2017-05-14 13:45:53 +0000

    Updated for better check of unix or natural date

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-/* TODO
-  1. Better check if it's a unix date
-  2. Return null if not a natural language date or a unix date
-*/
-
-
 var express = require("express");
 
 var app = express();
@@ -13,23 +7,29 @@ app.get('/:inputDate', function (req, res) {
   var natural;
   
   // Check if input is natual or unix
-  unix = parseInt(req.params.inputDate);
   
-  if (isNaN(unix)) {
+  if (req.params.inputDate.match(/^[0-9]+$/) != null) {
     
-    // If natural language date ...
-    unix = Date.parse(req.params.inputDate) / 1000;
-    natural = req.params.inputDate;
-    
-  } else {
-     
-    // If unix date ...
+    // ... if unix date ...
+    unix = parseInt(req.params.inputDate);
     natural = new Date(unix * 1000);
     var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var mm = natural.getMonth();
     var dd = natural.getDate();
     var yy = natural.getFullYear();
     natural = monthNames[mm] + ' ' + dd + ', ' + yy;
+    
+  } else if (!isNaN(Date.parse(req.params.inputDate))) {
+    
+    // ... if natural language date ...
+    unix = Date.parse(req.params.inputDate) / 1000;
+    natural = req.params.inputDate;
+    
+  } else {
+    
+    // ... if neither of the above ...
+    unix = null;
+    natural = null;
     
   }
 
